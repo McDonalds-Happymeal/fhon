a3b9fc295cf8884ae9802b5e97e017aeb532459c
McDonalds-HappyMeal <Peter.p.maltby@gmail.com>
2017-11-16 10:34:31 +0000

    responsive mainly finished just awaiting testing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 window.onload = function() {
     console.log("width: "+ document.body.clientWidth);
-    console.log("Height: "+ document.body.clientHeight);
+    console.log("Height: "+ document.documentElement.clientHeight);
 }
