a328c5bf550a7a1b2bf319b9955b085b7bf9ee26
McDonalds-HappyMeal <Peter.p.maltby@gmail.com>
2017-11-21 12:09:04 +0000

    quick cleanup and testing javascript code gen with multiple area's.

--- js/customiser.js
+++ js/customiser.js
@@ -8,36 +8,70 @@ var frontPanel = [
     {
         "id":1,
         "name":"Black Leather",
-        "price":5,
+        "price":0.99,
         "texture":"leatherBlack.jpg"
     },
     {
         "id":2,
         "name":"Blue Leather",
-        "price":5,
+        "price":0.99,
         "texture":"leatherBlue.jpg"
     },
     {
         "id":3,
         "name":"Red Leather",
-        "price":5,
+        "price":0.99,
         "texture":"leatherRed.jpg"
     },
     {
         "id":4,
         "name":"Aluminium",
-        "price":15,
+        "price":2.99,
+        "texture":"metal_silver.jpg"
+    },
+]
+
+var backPanel = [
+    {
+        "id":0,
+        "name":"Black Plastic",
+        "price":0,
+        "texture":"black.png"
+    },
+    {
+        "id":1,
+        "name":"Black Leather",
+        "price":4.99,
+        "texture":"leatherBlack.jpg"
+    },
+    {
+        "id":2,
+        "name":"Blue Leather",
+        "price":4.99,
+        "texture":"leatherBlue.jpg"
+    },
+    {
+        "id":3,
+        "name":"Red Leather",
+        "price":4.99,
+        "texture":"leatherRed.jpg"
+    },
+    {
+        "id":4,
+        "name":"Aluminium",
+        "price":9.99,
         "texture":"metal_silver.jpg"
     },
 ]
 
 var options = {
     "frontPanel":0,
-    "backpanel":0
+    "backPanel":0
 };
 
 window.onload = function() {
     Populate("frontPanel",frontPanel);
+    Populate("backPanel",backPanel);
 }
 
 function Populate(name,resourse) {
@@ -62,4 +96,5 @@ function selection(name, id) {
     console.log(eval("options."+name));
     eval("options."+name+"=id");
     //update();
+    //remove active tag from all selection and add active to location.id
 }
