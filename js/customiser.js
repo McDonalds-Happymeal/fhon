94ca95fc076b2863e7729be66827e768ebeadfef
McDonalds-HappyMeal <Peter.p.maltby@gmail.com>
2017-11-23 09:15:41 +0000

    minor change to footer and customiser js work

--- js/customiser.js
+++ js/customiser.js
@@ -1,3 +1,14 @@
+//This is the most disgusting code file i have ever made. it works; but at what cost.
+
+var device = [
+    {
+        "id":0,
+        "name":"Prime",
+        "price":200,
+        "texture":"prime.png"
+    },
+]
+
 var frontPanel = [
     {
         "id":0,
@@ -7,28 +18,35 @@ var frontPanel = [
     },
     {
         "id":1,
-        "name":"Black Leather",
-        "price":0.99,
-        "texture":"leatherBlack.jpg"
-    },
-    {
-        "id":2,
-        "name":"Blue Leather",
-        "price":0.99,
-        "texture":"leatherBlue.jpg"
-    },
-    {
-        "id":3,
-        "name":"Red Leather",
-        "price":0.99,
-        "texture":"leatherRed.jpg"
-    },
-    {
-        "id":4,
         "name":"Aluminium",
         "price":2.99,
         "texture":"metal_silver.jpg"
     },
+    {
+        "id":2,
+        "name":"Titanium",
+        "price":2.99,
+        "texture":"metalBlack.jpg"
+    },
+    {
+        "id":3,
+        "name":"Mahogony",
+        "price":2.99,
+        "texture":"waveRed.png"
+    },
+    {
+        "id":4,
+        "name":"Oak",
+        "price":2.99,
+        "texture":"woodDark.jpg"
+    },
+    {
+        "id":5,
+        "name":"Pine",
+        "price":2.99,
+        "texture":"woodYellow.jpg"
+    }
+
 ]
 
 var backPanel = [
@@ -41,19 +59,19 @@ var backPanel = [
     {
         "id":1,
         "name":"Black Leather",
-        "price":4.99,
+        "price":2.99,
         "texture":"leatherBlack.jpg"
     },
     {
         "id":2,
         "name":"Blue Leather",
-        "price":4.99,
+        "price":2.99,
         "texture":"leatherBlue.jpg"
     },
     {
         "id":3,
         "name":"Red Leather",
-        "price":4.99,
+        "price":2.99,
         "texture":"leatherRed.jpg"
     },
     {
@@ -62,9 +80,37 @@ var backPanel = [
         "price":9.99,
         "texture":"metal_silver.jpg"
     },
+    {
+        "id":5,
+        "name":"Titanium",
+        "price":9.99,
+        "texture":"metalBlack.jpg"
+    },
+    {
+        "id":6,
+        "name":"Maghogony",
+        "price":4.99,
+        "texture":"waveRed.png"
+    },
+    {
+        "id":7,
+        "name":"Oak",
+        "price":4.99,
+        "texture":"woodDark.jpg"
+    },
+    {
+        "id":8,
+        "name":"Pine",
+        "price":4.99,
+        "texture":"woodYellow.jpg"
+    },
 ]
 
 var options = [
+    {
+        "name":"device",
+        "selected":0
+    },
     {
         "name":"frontPanel",
         "selected":0
@@ -75,9 +121,14 @@ var options = [
     },
 ]
 
+var price = 0;
+
 window.onload = function() {
     Populate("frontPanel",frontPanel);
     Populate("backPanel",backPanel);
+    Populate("device",device);
+
+    update();
 }
 
 function Populate(name,resourse) {
@@ -86,8 +137,7 @@ function Populate(name,resourse) {
 
     for(var i = 0; i< resourse.length; i++) {
         var obj = resourse[i];
-            area.insertAdjacentHTML("beforeend",'<div class="option selected" onclick="selection(\''+name+'\','+obj.id+')" ><img src="/assets/customiser/textures/'+obj.texture+'" /><p>'+obj.name+'</p></div>');
-        
+            area.insertAdjacentHTML("beforeend",'<div class="option" onclick="selection(\''+name+'\','+obj.id+')" ><img src="/assets/customiser/textures/'+obj.texture+'" /><p>'+obj.name+'</p></div>');
         console.log(eval("options."+name));
     }
     //area.insertAdjacentHTML("beforeend","<p>some test html</p>")
@@ -102,10 +152,10 @@ function selection(name, id) {
     console.log(options);
     eval("options."+name+"=id");
     update();
-    //remove active tag from all selection and add active to location.id
 }
 
 function update() {
+    price = 0;
     var element = document.getElementsByClassName("selected");
     for(var i =0;i<element.length;i) {
         element[i].classList.remove("selected");
@@ -114,10 +164,6 @@ function update() {
     for(var i =0; i<options.length;i++) {
         obj = document.getElementById(options[i].name);
         obj = obj.getElementsByClassName("option")[options[i].selected];
-
         obj.className += " selected";
-
     }
-
-
 }
