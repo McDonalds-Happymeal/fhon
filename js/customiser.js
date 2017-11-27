f66c6af13ecf34423b1060bf0af9765a25e3e7b0
McDonalds-HappyMeal <Peter.p.maltby@gmail.com>
2017-11-27 15:21:19 +0000

    relabled images to be better used and started on json files

--- js/customiser.js
+++ js/customiser.js
@@ -1,3 +1,44 @@
 // v2.0 WOO!
 
+var defaults = [
+    {
+        "id":1,
+        "device":"Prime",
+    },
+    {
+        "device":"Prime",
+        "frontPanel":0,
+        "backPanel":0,
+        "text":null
+    },
+    {
+        "device":"Tuff",
+        "frontPanel":0,
+        "backPanel":0,
+        "text":null
+    },
+    {
+        "device":"Tuff",
+        "frontPanel":0,
+        "backPanel":0,
+        "text":null
+    }
+];
 
+window.onload = function() {
+    initialise();
+}
+
+function initialise() {
+    if (localStorage.getItem("options")===null) {
+        console.log("hello");
+        localStorage.setItem("options", JSON.stringify(defaults));
+    }
+
+    var options = JSON.parse(localStorage.getItem("options"));
+
+    console.log(options);
+    
+    
+
+}
