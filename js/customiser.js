8b7d58a8609043b79e6800f576590f19028d598d
McDonalds-HappyMeal <Peter.p.maltby@gmail.com>
2017-11-27 21:32:52 +0000

    i just broke lots of stuff; thats about it :)

--- js/customiser.js
+++ js/customiser.js
@@ -1,9 +1,8 @@
 // v2.0 WOO!
-
+//defualt JSON uploaded if user does not have already.
 var defaults = [
     {
-        "id":1,
-        "device":"Prime",
+        "id":0,
     },
     {
         "device":"Prime",
@@ -25,20 +24,69 @@ var defaults = [
     }
 ];
 
+var state = null; //global variable for my sanity; sorry :(
+
 window.onload = function() {
     initialise();
 }
 
+
+//intialise loads up customiser
 function initialise() {
+
+    //local storage 
     if (localStorage.getItem("options")===null) {
-        console.log("hello");
+        console.log("local storage loading attempted");
         localStorage.setItem("options", JSON.stringify(defaults));
     }
 
-    var options = JSON.parse(localStorage.getItem("options"));
+    state = JSON.parse(localStorage.getItem("options"));
+    //loads local stoage data for use in program.
+
 
     console.log(options);
     
-    
+    drawoptions();
 
+}
+
+
+//generates UI for customiser
+function drawoptions() {
+    var area = document.getElementById("options");
+    //area.innerHTML = "<!--wiped-->";
+    area.innerHTML = '<div id="device"><h3>device</h3><div class="select"></div></div>';
+    //area.insertAdjacentHTML("beforeend",'<div id="device"><h3>device</h3><div class="select"></div></div>');
+    console.log(state);
+
+    var numOptions = (device[state[0].id]).options;//hold number of options used for loops.
+    for(var i = 0; i< numOptions.length; i++) {
+        area.insertAdjacentHTML("beforeend",'<div id="'+device[state[0].id].options[i]+'"><h3>'+device[state[0].id].options[i]+'</h3><div class="select"></div></div>');
+    }
+
+    numOptions.unshift("device");//adds device top options array for generation
+
+
+    for(var i = 0; i<numOptions.length;i++) {//loops through each catgory generated above
+        area = document.getElementById(numOptions[i]);
+        area = area.getElementsByClassName("select")[0];
+
+        var resourse = eval(numOptions[i]);
+
+        for(var k = 0; k< resourse.length; k++) {//goes through json for catagory and populates options
+            var obj = resourse[k];
+                area.insertAdjacentHTML("beforeend",'<div class="option" onclick="selection(\''+numOptions[i]+'\','+obj.id+')" ><img src="/assets/customiser/textures/'+obj.texture+'" /><p>'+obj.name+'</p></div>');
+        }
+    }
+}
+
+function selection(name, id) {//html refers catagory and id to here for update.
+    if (name === "device") {//special case for device
+        console.log(id + " " + state[0].id);
+        state[0].id = id;
+        
+        drawoptions();
+    }else{
+
+    }
 }
