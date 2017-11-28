902d01f2964ed4f60e45e3e85a70d03f95897599
McDonalds-HappyMeal <Peter.p.maltby@gmail.com>
2017-11-28 12:15:46 +0000

    ui working!

--- js/customiser.js
+++ js/customiser.js
@@ -1,30 +1,26 @@
 // v2.0 WOO!
 //defualt JSON uploaded if user does not have already.
+//localStorage.clear();//to do add user button for this!
+
 var defaults = [
     {
         "id":0,
     },
     {
-        "device":"Prime",
-        "frontPanel":0,
-        "backPanel":0,
-        "text":null
+        "device":"prime",
+        "selected":[0,0]
     },
     {
-        "device":"Tuff",
-        "frontPanel":0,
-        "backPanel":0,
-        "text":null
+        "device":"tuff",
+        "selected":[0,0]
     },
     {
-        "device":"Tuff",
+        "device":"base",
         "frontPanel":0,
-        "backPanel":0,
-        "text":null
+        "selected":[0]
     }
 ];
 
-var state = null; //global variable for my sanity; sorry :(
 
 window.onload = function() {
     initialise();
@@ -35,18 +31,20 @@ window.onload = function() {
 function initialise() {
 
     //local storage 
-    if (localStorage.getItem("options")===null) {
+    if (localStorage.getItem("options")==null) {
         console.log("local storage loading attempted");
         localStorage.setItem("options", JSON.stringify(defaults));
     }
 
     state = JSON.parse(localStorage.getItem("options"));
     //loads local stoage data for use in program.
+    //global for my sanity sorry.
 
 
     console.log(options);
     
     drawoptions();
+
 
 }
 
@@ -74,19 +72,47 @@ function drawoptions() {
 
         for(var k = 0; k< resourse.length; k++) {//goes through json for catagory and populates options
             var obj = resourse[k];
-                area.insertAdjacentHTML("beforeend",'<div class="option" onclick="selection(\''+numOptions[i]+'\','+obj.id+')" ><img src="/assets/customiser/textures/'+obj.texture+'" /><p>'+obj.name+'</p></div>');
+                area.insertAdjacentHTML("beforeend",'<div class="option" onclick="selection(\''+(i)+'\','+obj.id+')" ><img src="/assets/customiser/textures/'+obj.texture+'" /><p>'+obj.name+'</p></div>');
         }
     }
+
+    update();
 }
 
 function selection(name, ref) {//html refers catagory and id to here for update.
     //debugger;
-    if (name == "device") {//special case for device
-        console.log(ref + " " + state[0].id);
+    console.log(state);
+    if (name == 0) {//special case for device
         state[0].id = ref;
         
         drawoptions();
     }else{
-
+        
+        state[state[0].id+1].selected[name-1] = ref;
+        
     }
+
+    update();
+}
+
+function update() {
+    var element = document.getElementsByClassName("selected");
+    for(var i =0;i<element.length;i) {
+        element[i].classList.remove("selected");
+    }
+
+    element = document.getElementById("device");
+    element = element.getElementsByClassName("option");
+    element[state[0].id].className += " selected";
+
+    var array = state[(state[0].id)+1].selected.slice();
+    //array.pop();
+
+    element = document.getElementById("options");
+    element = element.getElementsByClassName("select");
+    for (var i =1;i<=array.length;i++) {
+        element[i].getElementsByClassName("option")[array[i-1]].className += " selected";
+    }
+
+    localStorage.setItem("options", JSON.stringify(state));
 }
